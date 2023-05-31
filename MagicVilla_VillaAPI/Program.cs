0d698535675291bb51b37a06ab382c88e439e499
Ibrahim Youssef <ibrahimmohamed.nbis@gmail.com>
2023-05-31 16:07:32 +0300

    removed custom logging.

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,6 +1,4 @@
 
-using MagicVilla_VillaAPI.Logging;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,7 +10,6 @@ builder.Services.AddControllers(option=>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ILogging, LoggingV2>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
